02bddbb4e8626bc20b5f325779aded07854944f7
DevGaby <sgabrielcalixte@gmail.com>
2020-05-26 18:43:36 +0200

    feat/week-9
    implementation home page

--- miageWebApp/src/app/app-routing.module.ts
+++ miageWebApp/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AccueilComponent } from './accueil/accueil.component';
 
 
 const routes: Routes = [];
